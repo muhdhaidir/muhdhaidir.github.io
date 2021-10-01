ed2ab3e902df7581696d313d3ac8dcfa06fd4c74
Muhd Haidir <muhd.haidir1996@gmail.com>
2021-10-01 20:53:40 +0800

    added js

--- event-handler/js/event.js
+++ event-handler/js/event.js
@@ -6,3 +6,17 @@ let elNamaPengguna = document.getElementById("namaPengguna")
 elNamaPengguna.addEventListener("keyup",function(){
     elNamaPengguna.value = transformUppercase(elNamaPengguna.value)
 })
+
+function checkPassword(){
+    let elKataLaluan = document.getElementById("kataLaluan")
+    let pass_val = elKataLaluan.value
+
+    if(pass_val.length > 5){
+        alert("Password check...OK!")
+    } else {
+        alert("Password too short!")
+    }
+}
+
+let elKL = document.getElementById("kataLaluan")
+elKL.onblur = checkPassword
