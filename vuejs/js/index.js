21806fe9458ffef9a0d8f9900e14b38e8e14505b
Muhd Haidir <muhd.haidir1996@gmail.com>
2021-10-15 19:51:09 +0800

    added vue js

--- vuejs/js/index.js
+++ vuejs/js/index.js
@@ -44,6 +44,24 @@ let app4 = new Vue ({
 let app5 = new Vue ({
     el:"#app5",
     data: {
-        message:"Hello World!"
+        message:"Hello World!",
+        groceryList: [
+            {
+                id: 1,
+                desc: "Rocket Salad"
+            },
+            {
+                id: 2,
+                desc: "Cheese"
+            },
+            {
+                id: 3,
+                desc: "Ramen"
+            },
+            {
+                id: 4,
+                desc: "Ice Lemon Tea"
+            }
+        ]
     }
 })
